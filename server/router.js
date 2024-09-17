0e04ceecec1311ddefb71bd917308cb792bccc08
Jake <jakedavisemail@gmail.com>
2024-09-17 16:21:14 -0600

    router test

--- server/router.js
+++ server/router.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
 const controler = require('./controler.js');
 
-router.get('/access', (req, res) => {
-  const json = {
-    access: 'granted'
-  };
-  res.status(200).json(json);
-});
+router.get('/test', controler.test);
 
 module.exports = router;
